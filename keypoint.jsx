43b93c1fd6a1133b16f9f87f7341ce39c5612add
chromeheartz <barnesquiat@gmail.com>
2022-11-03 23:54:15 +0900

    [#2.3-#2.4] third-party / layout system

--- keypoint.jsx
+++ keypoint.jsx
@@ -63,3 +63,59 @@
   이제 AsyncStorage를 사용하려면 다른곳에서 다운받아 써야한다.
 */
 
+/*
+  #2.3 Third Party Packages
+
+  component와 api의 차이점
+
+  component는 화면에 렌더링할 항목이다
+
+  API는 자바스크립트 코드이다
+  자바스크립트 코드가 운영체제와 소통하는것을 안다.
+  예를들어 vibration은 디바이스를 진동시킨다
+  rn에서 가져와서 vibration에 있는 메소드들을 실행시킬 수 있다
+
+  * 실제앱에서는 api가 충분하지 않을 수 있다
+  예를들어 facebook 로그인, 비동기저장소 등이 필요할 수 있다.
+  문서를 찾아보면 community package를 찾아보라고한다
+
+  reactnative.directory에는 third-party 패키지와 API가 있다
+  전에는 rn팀이 asyncStorage를 제공했지만 현재는 제공하지않음
+
+  현재 만약 storage를 원하면 스스로 만들어내야한다.
+  요점은 storage를 위한것이 많아서 옵션을 잘 선택해야한다는것
+
+  *** expo 는 rn가 몇몇 package를 지원하지 않는것을 안다
+  그래서 expo는 자체적으로 packages와 api를 만들었는데 이것을 
+  expo SDK라고 한다
+  https://docs.expo.dev/versions/latest/
+  꽤많은 API들이 있다
+
+  이제 왜 Expo StatusBar와 RN StatusBar가 있는지 알았다
+  그 이유는 Expo가 RN의 일부 component,API를 복제하고 
+  개선하기로했기때문
+
+*/
+
+/*
+
+  #2.4 Layout system
+
+  RN의 flex는 web과 흡사하다
+  flex container가 되어라 라고 할필요 없다 display:flex를 안해도된다
+  default로 flex-direction의 방향이 column이다
+
+  기본적으로 모든 View는 flex container
+  overflow가 있다고 스크롤을 할 수 있는것도 아님
+
+  또다른 중요한 포인트는
+  너비와 높이에 기반해서 레이아웃을 만들지 않을것이다.
+  스크린 사이즈에 따라서 다르게 보이니 flex속성의 비율로 잡아주는것이 좋다
+
+  아이콘, 아바타 같은것이면 괜찮을텐데 레이아웃에서는 안쓸것이다
+
+  * 부모에 flex1을 주고, 자식들에게 flex1로 주면 화면을 3분할해서 꽉차서 들어가게된다
+  flex속성을 잘 조절해서 반응형으로 잡아주는것이 포인트
+  부모에 속성이 없으면 자식들이 어떤것을 기준으로 flex가 잡히는지 모른다. 
+
+*/
